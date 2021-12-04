7c56c727548f091c229544c4c3e8ffcc16ea6118
pcancio <pcancio@gmail.com>
2021-12-04 14:44:01 -0500

    commented out previous code to focus on dispalying CurrentDay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,62 @@
-function updateTimeSlotItems() {
-    // assign classes, WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
-    // read relevant local storage data
-    $('.hour').each(function() {
-        var hours = moment($(this).text().trim(), 'LT');
-        var duration = Math.ceil(moment.duration(hours.diff(moment())).asHours());
-        //  if duration is nega then is past 
-        if (parseInt(duration) < 0) {
-            $(this).next().addClass('past');
-        }
-        // if duration is 0 then current
-        if (parseInt(duration) === 0) {
-            $(this).next().addClass('present');
-        }
-        // otherwise is future
-        else {
-            $(this).next().addClass('future');
-        }
-    });
+// WHEN I open the planner THEN the current day is displayed at the top of the calendar
+var currentDay = $("#currentDay");
+var displayCurrentDay = moment().format("MMMM DD YYYY");
+currentDay.text(displayCurrentDay);
 
-    function update
+function textAreaColor() {
+    $('.time-block').each(function() {
+                var currentHour = parseInt($(this).attr('id'));
+                console.log(currentHour);
 
-    function handleSave(e) {
-        var hour = $(e.target).closest('.time-block').attr('id')
-        var value = $(e.target).siblings('.description'.val().trim() localStorage.setItem(hour, value)
-        }
+                if (currentHour < timeNow) {
+                    $(this).addClass('past')
+                } else if (currentHour === timeNow) {
+                    $(this).addClass('present')
+                } else {
+                    $(this).addClass('future')
+                }
+                textAreaColor();
 
-        function main() {
-            updateTimeSlotItems();
 
-            $(document).on('click', '.save-btn', handleSave)
+                // function updateTimeSlotItems() {
+                //     // assign classes, WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
+                //     // read relevant local storage data
+                //     $('.time-block').each(function() {
+                //         var hours = moment($(this).text().trim(), 'LT');
+                //         var duration = Math.ceil(moment.duration(hours.diff(moment())).asHours());
+                //         //  if duration is nega then is past 
+                //         if (parseInt(duration) < 0) {
+                //             $(this).next().addClass('.past');
+                //         }
+                //         // if duration is 0 then current
+                //         if (parseInt(duration) === 0) {
+                //             $(this).next().addClass('.present');
+                //         }
+                //         // otherwise is future
+                //         else {
+                //             $(this).next().addClass('.future');
+                //         }
+                //     });
 
-        }
+                //     // get local storage value
+                //     $('.description').each(function() {
+                //                 var hour = $(thi)
 
-        // start
-        $(main);
+                //                 //     var hour = $(e.target).closest('.time-block').attr('id');
+                //                 //     var value = $(e.target).siblings('.description'.val().trim().localStorage.setItem(hour, value)
+                //                 //     };
+
+                //                 // })
+
+                //                 // function handleSave(e) {
+
+
+
+                //                 // function main() {
+                //                 //     $(document).on('click', '.save-btn', handleSave)
+                //                 //     $('#currentDay').text(moment().format('dddd, LL'));
+                //                 //     updateTimeSlotItems();
+                //                 // };
+
+                //                 // // start
+                //                 // $(document).ready(main)
